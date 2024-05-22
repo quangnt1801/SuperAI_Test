a35d072d47177d486ab875ca04f6e08cde1d506c
Nguyễn Thành Quang <>
2024-05-22 18:21:53 +0700

    Print full tem with IOS and android

--- ios/TSCPrinterModule.swift
+++ ios/TSCPrinterModule.swift
@@ -26,7 +26,7 @@ class TSCPrinterModule: NSObject {
                 return
             }
         }
-        wireless.closeport(4)
+//        wireless.closeport(4)
         // print("Failed to open connection after 15 attempts.")
         completion(false)
     }
@@ -41,41 +41,27 @@ class TSCPrinterModule: NSObject {
         wireless.clearBuffer()
     }
   
-  
-  func printDotTop(numberDashes: Int, x: Int, y: Int) {
+  func horizontalDash(numberDashes: Int, x: Int, y: Int) {
     let numberOfDashes = numberDashes // Số lượng dấu gạch ngang, điều chỉnh theo nhu cầu của bạn
     let dashLine = String(repeating: "-", count: numberOfDashes)
     wireless.windowsfont(x, y: y, height: 22, rotation: 0, style: 4, withUnderline: 0, fontName: "Arial", content: dashLine)
   }
-//  
-//  func verticalDash(x: Int, y: Int, numberOfLines: Int) {
-//    let xPosition: Int = 14 // Vị trí X cố định
-//    let initialYPosition: Int = 14 // Vị trí Y ban đầu
-//    let yIncrement: Int = 20 // Khoảng cách Y giữa mỗi dấu |
-//    
-//    wireless.windowsfont(xPosition, y: 14, height: 20, rotation: 0, style: 3, withUnderline: 0, fontName: "Arial", content: "|")
-//    wireless.windowsfont(xPosition, y: 24, height: 20, rotation: 0, style: 3, withUnderline: 0, fontName: "Arial", content: "|")
-//    wireless.windowsfont(xPosition, y: 44, height: 20, rotation: 0, style: 3, withUnderline: 0, fontName: "Arial", content: "|")
-//    wireless.windowsfont(xPosition, y: 64, height: 20, rotation: 0, style: 3, withUnderline: 0, fontName: "Arial", content: "|")
-//    wireless.windowsfont(xPosition, y: 84, height: 20, rotation: 0, style: 3, withUnderline: 0, fontName: "Arial", content: "|")
-////    for i in 0..<numberOfLines {
-////        let yPosition = initialYPosition + (i * yIncrement)
-////        wireless.windowsfont(xPosition, y: yPosition, height: 20, rotation: 0, style: 3, withUnderline: 0, fontName: "Arial", content: "|")
-////    }
-//  }
   
-  
-  func verticalDash(x: Int, y: Int, numberOfLines: Int) {
-    let xPosition: Int = x // Vị trí X cố định
-    let initialYPosition: Int = y // Vị trí Y ban đầu
+  func verticalDash(numberOfLines: Int) -> String {
     let yIncrement: Int = 10 // Khoảng cách Y giữa mỗi dấu |
-
-    let numberOfLines: Int = numberOfLines // Số lượng dấu |
-
-    for i in 0..<numberOfLines {
-        let yPosition = initialYPosition + (i * yIncrement)
-        wireless.windowsfont(xPosition, y: yPosition, height: 4, rotation: 0, style: 10, withUnderline: 0, fontName: "Arial", content: "|")
-    }
+    var content: String = "|"
+    for i in 1..<numberOfLines {
+            content += "\n|"
+        }
+    return content
+  }
+  
+  func isTextLengthExceedingLimit(text: String) -> Bool {
+    let characterWidth: Int = 7;
+    let maxLength: Int = 380;
+    
+    let totalLength = text.count * characterWidth
+    return totalLength > maxLength
   }
  
     // Function to print a label with text and barcode
@@ -94,50 +80,82 @@ class TSCPrinterModule: NSObject {
       
       
         var PADDING_LEFT = 30;
-        var FONT_SIZE = 18;
+        var FONT_SIZE = 19;
+        var SIZE_BOLD = 19;
       
+        var leftDash = verticalDash(numberOfLines: 33)
+        var rightDash = verticalDash(numberOfLines: 33)
+        var qrDash = verticalDash(numberOfLines: 14)
       
-        //Print QRcode
-        let qrCommand = "QRCODE 470,60,L,4,A,0,\"\(qrcode)\"\r\n"
-        var a = wireless.sendcommand(qrCommand);
-        var b = wireless.windowsfont(464, y: 155, height: 18, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: labelQrcode)
-          
-          
-            //Print Barcode
-        var c = wireless.barcode("34", y: "38", barcodeType: "128", height: "60", readable: "0", rotation: "0", narrow: "2", wide: "1", code: barcode)
-        var d = wireless.windowsfont(70, y: 105, height: 28, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: labelBarcode)
-            
+        printQRCode(qrcode, label: labelQrcode)
 
-        var e = wireless.windowsfont(100, y: 155, height: 28, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: classificationCode)
-          
-        var f = wireless.windowsfont(PADDING_LEFT, y: 210, height: FONT_SIZE, rotation: 0, style: 1, withUnderline: 1, fontName: "Arial", content: address)
-        var g = wireless.windowsfont(PADDING_LEFT, y: 260, height: FONT_SIZE, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: infoShipping)
-          
-        var q = printDotTop(numberDashes: 78, x: 14, y: 15)
-        var w = printDotTop(numberDashes: 78, x: 14, y: 380)
-      
-      printDotTop(numberDashes: 60, x: 14, y: 136) //Dash classification
-      printDotTop(numberDashes: 78, x: 14, y: 180) //Dash address
-      
-      verticalDash(x: 14, y: 28, numberOfLines: 49)
-      verticalDash(x: 450, y: 28, numberOfLines: 16)
-      verticalDash(x: 580, y: 28, numberOfLines: 49)
+         // Print Barcode
+         printBarcode(barcode, label: labelBarcode)
 
-      
-      var h = wireless.windowsfont(PADDING_LEFT, y: 305, height: FONT_SIZE, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: infoProduct)
-      var j = wireless.windowsfont(PADDING_LEFT, y: 330, height: FONT_SIZE, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: contact)
-      var k = wireless.windowsfont(PADDING_LEFT, y: 355, height: FONT_SIZE, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: "Cho Thử Hàng")
-      var l = wireless.printlabel(1, copies: 1)
+         // Print Classification Code
+         wireless.windowsfont(120, y: 146, height: 28, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: classificationCode)
+
+         // Print Address
+         wireless.windowsfont(PADDING_LEFT, y: 195, height: FONT_SIZE, rotation: 0, style: 0, withUnderline: 0, fontName: "Arial", content: address)
+
+         // Print Info Shipping
+         let addressExceeds = isTextLengthExceedingLimit(text: address)
+         wireless.windowsfont(PADDING_LEFT, y: addressExceeds ? 244 : 224, height: SIZE_BOLD, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: infoShipping)
+
+         // Print Dashes
+      printDashes(left: leftDash, right: rightDash, qrDash: qrDash)
+
+         // Print Info Product
+        let contactExceeds = isTextLengthExceedingLimit(text: contact)
+        wireless.windowsfont(PADDING_LEFT, y: contactExceeds ? 300 : 316, height: FONT_SIZE, rotation: 0, style: 0, withUnderline: 0, fontName: "Arial", content: infoProduct)
+
+         // Print Contact
+         
+        wireless.windowsfont(PADDING_LEFT, y: contactExceeds ? 324 : 340, height: SIZE_BOLD, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: contact)
+
+         // Print "Cho Thử Hàng"
+         wireless.windowsfont(PADDING_LEFT, y: 364, height: FONT_SIZE, rotation: 0, style: 0, withUnderline: 0, fontName: "Arial", content: "Cho Thử Hàng")
+
+         // Print Label
+         wireless.printlabel(1, copies: 1)
+    }
+  
+  
+    func printQRCode(_ qrcode: String, label: String) {
+        let qrCommand = "QRCODE 475,50,L,4,A,0,\"\(qrcode)\"\r\n"
+        wireless.sendcommand(qrCommand)
+        wireless.windowsfont(473, y: 150, height: 18, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: label)
     }
 
-    @objc(initiatePrinting:labels:callback:) // Expose a method to React Native to initiate printing
+    func printBarcode(_ barcode: String, label: String) {
+        wireless.barcode("125", y: "33", barcodeType: "128", height: "60", readable: "0", rotation: "0", narrow: "2", wide: "1", code: barcode)
+        wireless.windowsfont(75, y: 95, height: 28, rotation: 0, style: 1, withUnderline: 0, fontName: "Arial", content: label)
+    }
+
+  func printDashes(left: String, right: String, qrDash: String) {
+        horizontalDash(numberDashes: 78, x: 14, y: 10) // Top dash
+        horizontalDash(numberDashes: 78, x: 14, y: 377) // Bottom dash
+        horizontalDash(numberDashes: 60, x: 14, y: 126) // Dash classification
+        horizontalDash(numberDashes: 78, x: 14, y: 170) // Dash address
+      wireless.windowsfont(12, y: 20, height: 10, rotation: 0, style: 10, withUnderline: 0, fontName: "Arial", content: left)
+      wireless.windowsfont(584, y: 20, height: 10, rotation: 0, style: 10, withUnderline: 0, fontName: "Arial", content: right)
+      wireless.windowsfont(450, y: 20, height: 10, rotation: 0, style: 10, withUnderline: 0, fontName: "Arial", content: qrDash)
+    }
+
+    func isTextLengthExceedingLimit(text: String, characterWidth: Int = 7, maxLength: Int = 380) -> Bool {
+        let totalLength = text.count * characterWidth
+        return totalLength > maxLength
+    }
+
+
+    @objc(initiatePrinting:labels:callback:) 
     func initiatePrinting(ip: String, labels: [[String: Any]], callback: @escaping RCTResponseSenderBlock) {
       openConnection(ip: ip) { success in
           if success {
               for labelData in labels {
                   self.printLabel(data: labelData)
               }
-              self.wireless.closeport(4)
+              self.wireless.closeport(1)
               callback(["Quá trình in hoàn tất thành công, đã in \(labels.count) nhãn."])
           } else {
               callback(["Kết nối với máy in thất bại. Quá trình in không được khởi động."])
--- src/screens/printerThermal/PrinterThermalScreen.tsx
+++ src/screens/printerThermal/PrinterThermalScreen.tsx
@@ -15,32 +15,36 @@ interface listBill {
     phoneUser: string,
     recipientName: string,
     shopName: string | any,
+    carrier_alias: string
 }
 
 const PrinterThermalScreen = () => {
 
     const { TSCPrinterModule } = NativeModules;
 
+    // 
     const [listBill, setListBill] = useState<listBill[]>([
         {
-            barCode: "BEST | 84857627495046",
+            barCode: "84857512884224",
             qrCode: "813464520",
             location: "24 Nguyễn Chánh Sắt, P.13, Q. Tân Bình, Hcm, Phường 13, Quận Tân Bình, Hồ Chí Minh",
             classificationCode: "SG071-00-003-02",
             phoneUser: '032****775',
             recipientName: 'Ng. Nhận: Hằng',
             shopName: 'THẢO HƯƠNG SPAI',
-            productType: "QUẦN ÁO, MỸ PHẨM"
+            productType: "QUẦN ÁO, MỸ PHẨM",
+            carrier_alias: "BEST"
         },
         {
-            barCode: "BEST | 84857512884224",
+            barCode: "84857512884224",
             qrCode: "813106845",
             location: "32 Thân Nhân Trung An Giang, Phường Đông Xuyên, Thành phố Long Xuyên, An Giang",
             classificationCode: "CT176-00-034-02",
             phoneUser: '091****796',
             recipientName: 'Ng. Nhận: Thảo',
             shopName: 'THẢO HƯƠNG SPAI',
-            productType: "QUẦN ÁO, MỸ PHẨM"
+            productType: "QUẦN ÁO, MỸ PHẨM",
+            carrier_alias: "BEST"
         },
     ]);
 
@@ -71,16 +75,28 @@ const PrinterThermalScreen = () => {
         height: 50,
     });
 
+    const verticalDash = (numberOfLines: number) => {
+        let content = "|";
+        for (let i = 1; i < numberOfLines; i++) {
+            content += "\n|";
+        }
+        return content
+    }
+
+
+    const horizontalDash = (numberDashes: number) => {
+        let dashLine = '-'.repeat(numberDashes);
+        return dashLine;
+    }
+
     const printerWithIOS = async () => {
         console.log("Printer@123");
-
-
         for (const item of listBill) {
 
             TSCPrinterModule.initiatePrinting("192.168.1.100", [{
                 barcode: item.barCode,
                 qrcode: item.qrCode,
-                labelBarcode: item.barCode,
+                labelBarcode: `${item.carrier_alias} | ${item.barCode}`,
                 labelQrcode: item.qrCode,
                 classificationCode: item.classificationCode,
                 address: insertLineBreaks(item.location),
@@ -92,26 +108,11 @@ const PrinterThermalScreen = () => {
 
             });
         }
-
-        // TSCPrinterModule.initiatePrinting("192.168.1.100", [{
-        //     barcode: 'BEST | 84857512884224',
-        //     qrcode: "813106845",
-        //     labelBarcode: "BEST | 84857512884224",
-        //     labelQrcode: "813106845",
-        //     classificationCode: "CT176-00-034-02",
-        //     address: '32 Thân Nhân Trung An Giang, Phường Đông Xuyên,\nThành phố Long Xuyên, An Giang',
-        //     infoShipping: "032****775 - Ng. Nhận: Hằng - ĐH của shop THẢO HƯƠNG SPAI",
-        //     infoProduct: "SP Cần Giao: QUẦN ÁO , MỸ PHẨM"
-        // }], (response: string) => {
-        //     Alert.alert('Thông báo', response);
-        //     // setLoading(false);
-        // });
-
     }
 
     const insertLineBreaks = (text: string) => {
 
-        const maxLength = 380;
+        const maxLength = 350;
         const charLength = 7;
         let currentLength = 0;
         let lastSpaceIndex = -1;
@@ -142,101 +143,235 @@ const PrinterThermalScreen = () => {
         return result;
     };
 
+    const isTextLengthExceedingLimit = (text: String) => {
+        const characterWidth = 7;
+        const maxLength = 380;
+
+        let totalLength = text.length * characterWidth
+        return totalLength > maxLength
+    }
+
     const printerWithAndroid = async () => {
-        const fontArial = await getPathTextFont('Arial');
-        const fontArialBold = await getPathTextFont('arialceb')
+        const fontArial = await getPathTextFont('ArialCEMTBlack');
+        const fontArialBold = await getPathTextFont('ArialCEMTBlack')
+
+        const leftDash = verticalDash(27);
+        const rightDash = verticalDash(27);
+        const qrDash = verticalDash(11);
+
+        const topDash = horizontalDash(57);
+        const bottomDash = horizontalDash(57);
+        const classificationDash = horizontalDash(44);
+        const addressDash = horizontalDash(57);
+
+        const FONT_SIZE = 21;
 
         await printer.open()
         await printer.clear()
         await printer.setup()
 
-
         for (const item of listBill) {
+
+
+            await printer.windowfont({
+                x: 14,
+                y: 24,
+                size: 12,
+                path: fontArial,
+                text: leftDash
+            })
+
+            await printer.windowfont({
+                x: 584,
+                y: 24,
+                size: 12,
+                path: fontArial,
+                text: rightDash
+            })
+
+            await printer.windowfont({
+                x: 450,
+                y: 24,
+                size: 12,
+                path: fontArial,
+                text: qrDash
+            })
+
+            //3 cột này cần khởi tạo đầu tiên vì Border của nó có thể che content 
+
             await printer.barcode({
-                x: 25,
-                y: 40,
+                x: 50 * 2.5,
+                y: 30,
                 height: 60,
-                narrow: 2,
+                narrow: 2.2,
                 wide: 1,
-                printText: true,
+                printText: false,
                 rotation: 0,
                 type: '128',
                 code: item.barCode
             })
+            await printer.windowfont({
+                x: 85,
+                y: 92,
+                size: 26,
+                path: fontArial,
+                text: `${item.carrier_alias} | ${item.barCode}`
+            })
+
             const qrCodeCommand = `
                 SIZE 5,2
-                QRCODE 450,80,L,3,A,0,"${item.qrCode}"
+                QRCODE 475,50,L,4,A,0,"${item.qrCode}"
             `;
             await printer.cmd(qrCodeCommand)
 
             await printer.windowfont({
-                x: 440,
-                y: 155,
+                x: 470,
+                y: 148,
                 size: 18,
                 path: fontArial,
                 text: item.qrCode
             })
 
             await printer.windowfont({
-                x: 120,
-                y: 155,
+                x: 135,
+                y: 140,
                 size: 28,
                 path: fontArialBold,
                 text: item.classificationCode
             })
 
+            let addressExceeds = isTextLengthExceedingLimit(item.location)
             await printer.windowfont({
-                x: 14,
-                y: 220,
-                size: 18,
+                x: 30,
+                y: 185,
+                size: FONT_SIZE,
                 path: fontArial,
                 text: insertLineBreaks(item.location)
             })
-            18
-            const textLines = [
-                { text: `${item.phoneUser} - `, font: fontArial, bold: false },
-                { text: item.recipientName, font: fontArialBold, bold: true },
-                { text: '- ĐH của shop ', font: fontArial, bold: false },
-                { text: item.shopName, font: fontArialBold, bold: true },
-            ];
-
-            let currentX = 14;
-            for (const line of textLines) {
-                await printer.windowfont({
-                    x: currentX,
-                    y: 280,
-                    size: 18,
-                    path: line.font,
-                    text: line.text
-                });
-
-                currentX += line.text.length * 9;
-            }
-
 
             await printer.windowfont({
-                x: 14,
-                y: 320,
+                x: 30,
+                y: addressExceeds ? 238 : 215,
+                size: FONT_SIZE,
+                path: fontArial,
+                text: insertLineBreaks(`${item.phoneUser}${item.recipientName}- ĐH của shop ${item.shopName}`)
+            })
+
+            let contactExceeds = isTextLengthExceedingLimit("KHÔNG GIAO ĐƯỢC, GỌI NGAY 0902644227. CẢM ƠN AE SHIPPER.")
+            await printer.windowfont({
+                x: 30,
+                y: contactExceeds ? 295 : 315,
                 size: 18,
                 path: fontArial,
                 text: `SP Cần Giao: ${item.productType}`
             })
 
             await printer.windowfont({
-                x: 14,
-                y: 346,
+                x: 30,
+                y: contactExceeds ? 320 : 340,
                 size: 18,
-                path: fontArialBold,
-                text: `KHÔNG GIAO ĐƯỢC, GỌI NGAY 0902644227. CẢM ƠN AE SHIPPER.`
+                path: fontArial,
+                text: insertLineBreaks('KHÔNG GIAO ĐƯỢC, GỌI NGAY 0902644227. CẢM ƠN AE SHIPPER.')
             })
 
             await printer.windowfont({
-                x: 14,
-                y: 372,
+                x: 30,
+                y: 364,
                 size: 18,
                 path: fontArial,
-                text: `Cho Thử Hàng`
+                text: insertLineBreaks("Cho Thử Hàng")
             })
+
+            // const textLines = [
+            //     { text: `${item.phoneUser} - `, font: fontArial, bold: false },
+            //     { text: item.recipientName, font: fontArialBold, bold: true },
+            //     { text: '- ĐH của shop ', font: fontArial, bold: false },
+            //     { text: item.shopName, font: fontArialBold, bold: true },
+            // ];
+
+            // let currentX = 14;
+            // for (const line of textLines) {
+            //     await printer.windowfont({
+            //         x: currentX,
+            //         y: 280,
+            //         size: 18,
+            //         path: line.font,
+            //         text: line.text
+            //     });
+
+            //     currentX += line.text.length * 9;
+            // }
+
+
+            // await printer.windowfont({
+            //     x: 14,
+            //     y: 320,
+            //     size: 18,
+            //     path: fontArial,
+            //     text: `SP Cần Giao: \n${item.productType}`
+            // })
+
+            // await printer.windowfont({
+            //     x: 14,
+            //     y: 346,
+            //     size: 18,
+            //     path: fontArialBold,
+            //     text: `KHÔNG GIAO ĐƯỢC, GỌI NGAY 0902644227. CẢM ƠN AE SHIPPER.`
+            // })
+
+            // await printer.windowfont({
+            //     x: 14,
+            //     y: 372,
+            //     size: 18,
+            //     path: fontArial,
+            //     text: `Cho Thử Hàng`
+            // })
+
+
+
+
+            await printer.text({
+                font: "1",
+                rotation: 0,
+                text: topDash,
+                x: 14,
+                y: 20,
+                zoomX: 1,
+                zoomY: 1
+            })
+
+            await printer.text({
+                font: "1",
+                rotation: 0,
+                text: bottomDash,
+                x: 14,
+                y: 394,
+                zoomX: 1,
+                zoomY: 1
+            })
+
+            await printer.text({
+                font: "1",
+                rotation: 0,
+                text: classificationDash,
+                x: 14,
+                y: 126,
+                zoomX: 1,
+                zoomY: 1
+            })
+
+            await printer.text({
+                font: "1",
+                rotation: 0,
+                text: addressDash,
+                x: 14,
+                y: 170,
+                zoomX: 1,
+                zoomY: 1
+            })
+
+
+
 
             await printer.print(1, 1)
             await printer.clear()
@@ -248,11 +383,11 @@ const PrinterThermalScreen = () => {
 
 
     const onPrinterThermal = async () => {
-        printerWithIOS();
-        // if (Platform.OS === 'ios') {
-        // } else {
-        //     printerWithAndroid();
-        // }
+        if (Platform.OS === 'ios') {
+            printerWithIOS();
+        } else {
+            printerWithAndroid();
+        }
     }
 
     const renderItem = ({ item, index }: { item: listBill, index: number }) => {
